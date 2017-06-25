b70889231285ca3234d14f938bad5b8cee308f13
Fang Hau <cozyazure@hotmail.com>
2017-06-25 21:06:01 +0800

    done with checkboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  public tiles = [];
+  public knightCurrentPos = {
+    x: 7,
+    y: 0
+  }
+
+  constructor() {
+    for (let i = 0; i < 8; i++) {
+      const temp = [];
+      for (let j = 0; j < 8; j++) {
+        const tile = new Tile(i, j);
+        temp.push(tile);
+      }
+      this.tiles.push(temp);
+    }
+
+  }
+
+  knightIsHere(tile: Tile) {
+    return tile.x === this.knightCurrentPos.x && tile.y === this.knightCurrentPos.y;
+  }
+}
+
+
+export class Tile {
+  public x: number;
+  public y: number;
+  public isVisited: boolean;
+
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
 }
